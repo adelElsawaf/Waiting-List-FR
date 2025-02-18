3eacf5938161addc2b763a9bb39b6f67aa57ddab
adelElsawaf <adelsadeq24@gmail.com>
2025-02-18 21:33:11 +0200

    fix production

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // ✅ Uses SWC minifier to reduce JS size
-  webpack(config) {
-    config.optimization.splitChunks = {
-      chunks: "all",
-      maxSize: 2000000, // ✅ Limit chunk size to 2MB (Cloudflare limit is 25MB)
-    };
-    return config;
-  },
+  productionBrowserSourceMaps: false, // ✅ Disables source maps to reduce build size
 };
 
 export default nextConfig;
